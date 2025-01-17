a444e331e08b109125fe384cb8e807a948cf3254
jmoreno-25 <jhonelito-mmpp@hotmail.com>
2025-01-17 03:29:36 -0500

    prueba conexion

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false, // Para producción
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: false, // Necesario en Railway
+    },
   });
 
 const crypto = require('crypto');
